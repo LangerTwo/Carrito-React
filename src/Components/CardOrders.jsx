63dd760fcbbf693977c6f44f3c693be4fed70040
LangerTwo <adrangel95@gmail.com>
2024-04-06 16:50:45 -0500

    creando CardOrders

--- src/Components/CardOrders.jsx
+++ src/Components/CardOrders.jsx
@@ -1,27 +1,16 @@
-// import { BsX } from "react-icons/bs";
+// // import {  } from "react-icons/bs";
 
-// const OrderCard = (props) => {
-//     const { id, title, imageUrl, price, handleDelete } = props
-//     let renderBsX
-//     if (handleDelete) {
-//         renderBsX = <div className="bg-red-500 rounded-full cursor-pointer">
-//                         <BsX onClick={() => handleDelete(id)} className="text-2xl text-white"/>
-//                     </div>
-//     }
+// const CardOrders = (props) => {
+//     const { totalPrice, totalProducts } = props
+
 //   return (
 //     <div className='flex justify-between items-center px-2 mb-2'>
-//         <div className="flex items-center gap-2">
-//             <figure className="w-20 h-20">
-//                 <img className="w-full h-full rounded-lg object-cover" src={imageUrl} alt={title} />
-//             </figure>
-//             <p className="text-sm font-light">{title}</p>
-//         </div>
-//         <div className="flex items-center gap-2">
-//             <p className="text-lg font-medium">{price}</p>
-//             {renderBsX}
-//         </div>
+//         <p>
+//             <span>06/04/2024</span>
+//             <span>{totalProducts}</span>
+//         </p>
 //     </div>
 //   )
 // }
 
-// export default OrderCard
+// export default CardOrders
