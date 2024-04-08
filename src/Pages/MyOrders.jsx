b7d2653d01df449c7b277f037e26d19e1bcb3573
LangerTwo <adrangel95@gmail.com>
2024-04-08 16:26:16 -0500

    page myorder y myorders hecha

--- src/Pages/MyOrders.jsx
+++ src/Pages/MyOrders.jsx
@@ -8,16 +8,18 @@ function MyOrders() {
     const context = useContext(CartContext)
     return (
         <Layout>
-            MyOrders
+            <div className="mb-5">
+                <h1>My Orders</h1>
+            </div>
             {
-                context.order.map((order, index) => {
-                    <Link key={index} to={`/my-orders${order.id}`}>
+                context.order.map((order, index) => (
+                    <Link key={index} to={`/my-orders/${index}`}>
                         <CardOrders 
                             totalPrice={order.totalPrice}
                             totalProducts={order.totalProducts}
                         />
                     </Link>
-                })
+                ))
             }
         </Layout>
     )
